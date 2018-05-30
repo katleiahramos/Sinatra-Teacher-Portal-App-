8155b020df8e34e470d5d46075d0e33d2df79f86
Katleiah Ramos <katleiahramos@gmail.com>
2018-05-30 16:27:27 +0000

    add seed file

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+Teacher.create(:name=> "Katleiah", :username => "kat", :password => "awesome")
+Teacher.create(:name=> "Matt", :username => "matt", :password => "yay")
+Teacher.create(:name=> "Lisa", :username => "lisa", :password => "coolio")
+
+
+
+Student.create(:name => "Alex")
+Student.create(:name => "John")
+Student.create(:name => "Everett")
+Student.create(:name => "Sara")
